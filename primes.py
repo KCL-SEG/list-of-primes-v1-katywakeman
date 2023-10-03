90f41d57bc53fccbb9632eaa27f57cd5a076cfa3
katywakeman <124585957+katywakeman@users.noreply.github.com>
2023-10-03 22:31:00 +0100

    Add functionality

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    count_primes = 0
+    running_count = 2
+    while count_primes < number_of_primes:
+        i = running_count - 1
+        mods = 0
+        while i > 0:
+            if (running_count % (running_count - i)) != 0:
+                mods += 1
+            i -= 1
+        if mods == (running_count - 2):
+            count_primes += 1
+            list.append(running_count)
+        running_count += 1
     return list
